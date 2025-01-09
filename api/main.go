77981ee0f738ad9cb5780835a33b1dbb7c812ebd
James Player <james.player@gmail.com>
2025-01-09 23:21:25 +1300

    Added config as volume

--- api/main.go
+++ api/main.go
@@ -10,23 +10,46 @@ import (
 type Response struct {
 	Msg string `json:"msg"`
 	Env map[string]string `json:"env"`
+	Config map[string]string `json:"config"`
+}
 
-	
+func getFileContents(filePath string) (string, error) {
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
 }
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		color, err := getFileContents("/etc/app-config/color")
+		if err != nil {
+			fmt.Fprintf(w, "Error getting color config")
+			return
+		}
+
+		logoUrl, err := getFileContents("/etc/app-config/logo_url")
+		if err != nil {
+			fmt.Fprintf(w, "Error getting logo_url config")
+			return
+		}
+		
 		json, err := json.Marshal(Response{
-			Msg: "Hit API server",
+			Msg: "Hit API server.",
 			Env: map[string]string{
 				"MY_NODE_NAME": os.Getenv("MY_NODE_NAME"),
 				"MY_POD_NAME": os.Getenv("MY_POD_NAME"),
 				"MY_POD_IP": os.Getenv("MY_POD_IP"),
 				"MY_SECRET": os.Getenv("MY_SECRET"),
 			},
+			Config: map[string]string{
+				"color": color,
+				"logoUrl": logoUrl,
+			},
 		})
 
-		if (err != nil) {
+		if err != nil {
 			fmt.Fprintf(w, "Error marshalling response into json")
 			return
 		}
