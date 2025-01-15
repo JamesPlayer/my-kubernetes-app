0f0c5bb4bcc90e757e47130ce05494dac9fdf6a8
James Player <james.player@gmail.com>
2025-01-15 17:03:32 +1300

    Microservice call

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,13 @@
 module github.com/JamesPlayer/my-kubernetes-app/api
 
 go 1.23.4
+
+require (
+	github.com/JamesPlayer/my-kubernetes-app/microservice v0.0.0-20250115020002-759b71441e28 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/grpc v1.69.4 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
+
+	pb "github.com/JamesPlayer/my-kubernetes-app/microservice/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+var addr = flag.String("addr", "localhost:50051", "address of microservice")
+
 type Response struct {
-	Msg string `json:"msg"`
-	Env map[string]string `json:"env"`
+	Msg    string            `json:"msg"`
+	Env    map[string]string `json:"env"`
 	Config map[string]string `json:"config"`
 }
 
@@ -22,7 +33,26 @@ func getFileContents(filePath string) (string, error) {
 }
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	flag.Parse()
+
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	microserviceClient := pb.NewPingPongServiceClient(conn)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Contact the microservice and print out its response.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		microserviceReply, err := microserviceClient.Ping(ctx, &pb.PingPongRequest{Message: "Ping"})
+		if err != nil {
+			log.Fatalf("could not ping: %v", err)
+		}
+		log.Printf("Ping: %s", microserviceReply.GetMessage())
+
 		color, err := getFileContents("/etc/app-config/color")
 		if err != nil {
 			fmt.Fprintf(w, "Error getting color config")
@@ -34,17 +64,17 @@ func main() {
 			fmt.Fprintf(w, "Error getting logo_url config")
 			return
 		}
-		
+
 		json, err := json.Marshal(Response{
 			Msg: "Hit API server.",
 			Env: map[string]string{
 				"MY_NODE_NAME": os.Getenv("MY_NODE_NAME"),
-				"MY_POD_NAME": os.Getenv("MY_POD_NAME"),
-				"MY_POD_IP": os.Getenv("MY_POD_IP"),
-				"MY_SECRET": os.Getenv("MY_SECRET"),
+				"MY_POD_NAME":  os.Getenv("MY_POD_NAME"),
+				"MY_POD_IP":    os.Getenv("MY_POD_IP"),
+				"MY_SECRET":    os.Getenv("MY_SECRET"),
 			},
 			Config: map[string]string{
-				"color": color,
+				"color":   color,
 				"logoUrl": logoUrl,
 			},
 		})
@@ -55,7 +85,7 @@ func main() {
 		}
 
 		fmt.Fprintf(w, "%s", string(json))
-    })
+	})
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
